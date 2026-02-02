f67418ec6262af956b0bee2bbfc0d1b1c78c2a20
Kenny <132385889+kennethrcunn@users.noreply.github.com>
2026-02-01 18:20:36 -0800

    Refactor movie parsing and menu display functions

--- cunniken_assignment2.c
+++ cunniken_assignment2.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
   printf("Processed file %s and parsed data for %d movies\n\n", argv[1], count);
 
   int choice;
-  do {                                                                            //
+  do {                                                                            //Prompt for menu options
     showMenu();
     printf("\nEnter a choice from 1 to 4: ");
     scanf("%d", &choice);
@@ -95,8 +95,96 @@ int main(int argc, char *argv[]) {
 }
 
 
-  
-  
+
+/**/
+void showMenu(void) {
+  printf("\n1. Show movies released in the specified year\n");
+  printf("2. Show highest rated movie for each year\n");
+  printf("3. Show the title and year of release of all movies in a specific language\n");
+  printf("4. Exit from the program\n"); 
+}
+
+struct movie *parseMovie(char *line) {
+  struct movie *m = malloc(sizeof(struct movie));
+  m -> next = NULL;
+
+  char *saveptr;
+  char *token = strtok_r(line, ", ", &saveptr);
+
+  m -> title = strdup(token);
+  m -> year = atoi(strtok_r(NULL, ", ", &saveptr));
+
+  char *langs = strtok_r(NULL, ", ", &saveptr);                                    // Parses languages
+  langs++;
+  langs[strlen(langs) - 1] = 0
+
+  m -> langCount = 0;
+  char *langToken;
+  char *langSave;
+  langToken = strtok_r(langs, ";", &langSave);
+
+  while (langToken && m -> langCount < max_langs) {
+    m -> languages[m -> langCount++] = strdup(langToken);
+    langToken = strtok_r(NULL, ",", &langSave);
+  }
+  m -> rating = strtof(strtok_r(NULL, ", ', &saveptr), NULL);
+    return m;
+}
+
+void moviesBYYear(struct movie *head, int year) {
+  int found = 0;
+  while (head) {
+    if (head -> year == year) {
+      printf("%s\n", head -> title);
+      found = 1;
+    }
+    head = head -> next;
+  }
+  if (found)
+    printf("No data about movies released in the year %d\n", year);
+}
+
+void highestRatedBYYear(struct movie *head) {
+  struct movie *best[300] = {NULL};
+
+  while (head) {
+    int y = head -> year;
+    if (!best[y] || head -> rating > best[y} -> rating)
+      best[y] = head;
+    head = head -> next;
+  }
+  for (int i = 1900; i <= 2021; i++) {
+    if (best[i])
+      printf("%d %.1f %s\n", i, best[i] -> rating, best[i] -> title);
+  }
+}
+
+void moviesByLanguage(struct movie *head, char *language) {
+  int found = 0;
+  while (head) {
+    for (int i = 0; i < head -> langCount; i++) {
+      if (strcmp(head -> languages[i], language) == 0) {
+        printf("%d %s \n", head -> year, head -> title);
+        found = 1;
+        break;
+      }
+    }
+    head = head -> next;
+  }
+  if (!found)
+    printf("No data about movies released in %s\n", language);
+}
+
+void freeMovies(struct movie *head) {
+  while (head) {
+    struct movie *tmp = head;
+    free(head -> title);
+    for (int i = 0; i < head ->langCount; i++)
+      free(head -> languages[i]);
+    head = head -> next;
+    free(tmp);
+  }
+}
 
 
 
